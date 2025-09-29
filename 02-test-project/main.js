ab5bed7dbf9ce549320416bf8f8aeaf73b2c6637
SValenziano <senorvalenz@gmail.com>
2025-09-29 17:40:25 -0400

    minor readme updates

--- 02-test-project/main.js
+++ 02-test-project/main.js
@@ -1,5 +1,5 @@
-// const {hello} = require("snv_npm_test");
-import {hello, world} from "snv_npm_test"
+// const {hello} = require("snv_npm_test"); // import as CommonJS / Node module
+import {hello, world} from "snv_npm_test";  // import as ES module
 
 hello();
 world();
